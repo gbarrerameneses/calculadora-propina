d642ce803bda9b92d3a6f35553e8bc7bd07c0976
Gustavo Barrera <gustavo.8805@gmail.com>
2024-03-28 13:34:45 -0600

    añadiendo elementos al stateOrder y validación de items duplicados

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -4,9 +4,27 @@ import type { MenuItem, OrderItem } from "../types";
 export default function userOrder() {
     const [order, setOrder] = useState<OrderItem[]>([]) // Generic Type
 
+    // Función para agregar producto del menú al consumo
     const addItem = (item : MenuItem) => { // tipando la función desde types
-        console.log('Agregando...', item);
+        // utilizo find porque es un array y busco que no haya un producto duplicado
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+        if (itemExist) {
+            // Identificando el elemento duplicado con map que nos da un nuevo arreglo
+            const updatedOrder = order.map( orderItem => 
+                orderItem.id === item.id // comparamos por medio de id's
+                ? {...orderItem, quantity: orderItem.quantity + 1} // toma una copia de lo que hay en la orden y toma lo que tenemos en quantity e incrementa en 1
+                : orderItem
+                )
+                setOrder(updatedOrder)
+
+        } else {
+            // const newItem : OrderItem = {...item, quantity: 1} // podríamos castear la variable con OrderItem, ya que estamos haciendo una copia del objeto
+            const newItem = {...item, quantity: 1}
+            setOrder([...order, newItem]) // pasamos la copia de ese ojeto a la función setOrder
+        }
     }
+
+    console.log(order);
 
     return {
         addItem
